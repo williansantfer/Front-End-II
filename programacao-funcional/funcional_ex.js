95da0063a0814cb4db8ae73bb689de3b14678136
Willian Ferreira dos Santos <willsantfer@gmail.com>
2024-10-23 21:50:47 -0300

    funcional_ex.js editado

--- programacao-funcional/funcional_ex.js
+++ programacao-funcional/funcional_ex.js
@@ -91,3 +91,62 @@ function incrementarPor(quantidade) {
 let incrementarCinco = incrementarPor(5);
 
 console.log(incrementarCinco(10));
+
+// Ex. 7
+
+function fibonacci(n) {
+
+    let cache = {};
+
+    function fib(n) {
+
+        if (n in cache) {
+            return cache[n];
+        }
+
+        if (n <= 1) {
+            return n;
+        }
+
+        cache[n] = fib(n - 1) + fib(n - 2);
+        return cache[n];
+    }
+    return fib(n);
+
+}
+
+const fib = fibonacci;
+
+console.log(fib(3));
+console.log(fib(6));
+console.log(fib(8));
+console.log(fib(8));
+
+// Ex. 8
+
+function potencia() {
+
+    let cache = {};
+
+    return function exp(base, expoente) {
+        
+        let chave = `${base},${expoente}`;
+
+        if (chave in cache) {
+            return cache[chave];
+        }
+
+        if (expoente === 0) {
+            return 1;
+        }
+
+        cache[chave] = base * exp(base, expoente - 1);
+        return cache[chave];
+    }
+}
+
+const pow = potencia();
+
+console.log(pow(2,4));
+console.log(pow(3,3));
+console.log(pow(3,3));
